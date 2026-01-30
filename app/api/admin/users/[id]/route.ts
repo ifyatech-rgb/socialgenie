8420fc9090103266e40600beb61508e249a15d79
ifyatech-rgb <ifyatech@gmail.com>
2026-01-30 11:10:36 +0530

    Fix authOptions export in NextAuth route

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
+import { authOptions } from "../../../../../lib/auth"
 import { createClient } from "@/lib/supabase/server"
 
 export async function GET(
